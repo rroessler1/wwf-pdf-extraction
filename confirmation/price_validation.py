60a022d0dd03a290eff5418ca90d39a726ac861c
DaryaZare <daryaz2079@gmail.com>
2024-11-05 21:11:06 +0100

    update price checker added

--- confirmation/price_validation.py
+++ confirmation/price_validation.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 def price_valudator(data):
     data.fillna('1',inplace=True)
-    return ['Passed' if data.discount_price/data.percentage_discount== data.original_price else 'Failed']
+    data['discount_price'] = data['discount_price'].astype(float)
+    data['percentage_discount'] = data['percentage_discount'].astype(float)
+    data['original_price'] = data['original_price'].astype(float)
+    
+    return ['Passed' if data.discount_price/data.percentage_discount - data.original_price < 0.05 else 'Failed']
