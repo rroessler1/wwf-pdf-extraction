85aa9e0dc171c8c313a61e36d9dc732f31d68027
DaryaZare <daryaz2079@gmail.com>
2024-11-19 15:37:49 +0100

    change setting to default

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,5 @@
-PDF_DIR = "pdf-files2"
+PDF_DIR = "pdf-files"
 API_KEY_PATH = "openai_api_key.txt"
 URL = "https://drive.google.com/drive/folders/1AR2_592V_x4EF97FHv4UPN5zdLTXpVB3"
-NUMBER_OF_CHATGPT_VALIDATIONS = 7
+NUMBER_OF_CHATGPT_VALIDATIONS = 2
 EXTRACTED_DATA_COLUMNS = ["product_name", "original_price", "discount_price","percentage_discount"]
